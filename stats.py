c6412260f418a65f357b8673d8b7457c71e211d2
Jamaine Roseborough <jroseborough31@gmail.com>
2017-12-25 18:46:15 -0800

    Initial commit

---
+++ stats.py
@@ -0,0 +1,80 @@
+import math
+
+def mean(x):
+	x = sorted(x)
+	return sum(x)/len(x)
+
+def median(x):
+	#determines if length of set is even or odd
+	x = sorted(x)
+	place = float(len(x))/float(2)
+
+	'''if length of set is odd, dviding by two will give a x.5
+	take away the .5 to get the place in the set that the median is'''
+	if place.is_integer == False:
+		return x[place-0.5]
+	#if length is even, get average of two closest medians
+	else:
+		return float(x[int(place)]+x[int(place)-1])/2
+
+def mode(x):
+	x = sorted(x)
+	freq = {}
+
+	'''iterates through list and gives key value pair
+	key being the instance in the list, value being 
+	number of times that instance appears in the list'''
+	for i in x:
+		n = 0
+		for e in x:
+			if e == i:
+				n+=1
+		freq.update({i:n})
+	return max(freq, key=freq.get)
+	#PROBLEM: ONLY RETURNS ONE KEY, POSSIBLE >1 KEY WITH EQUAL MAX VALUES
+
+
+def pop_var(x):
+	x = sorted(x)
+	num = x
+
+	#creates mean of data set
+	mean = sum(num)/len(num)
+
+	'''initializes new data set made up of the
+	data points after (Xi-mean)^2 has been ran'''
+	new_set = []
+
+	#runs calculation and add it to new_set
+	for i in num:
+		new_set.append((i - mean)**2)
+
+	#sum of new set divided by N(number of numbers in that set)
+	variance = float(sum(new_set))/float(len(new_set))
+	return variance
+
+def samp_var(x):
+	x = sorted(x)
+	num = x
+
+	#creates mean of data set
+	mean = sum(num)/len(num)
+
+	'''initializes new data set made up of the
+	data points after (Xi-mean)^2 has been ran'''
+	new_set = []
+
+	#runs calculation and add it to new_set
+	for i in num:
+		new_set.append((i - mean)**2)
+
+	#sum of new set divided by N(number of numbers in that set)
+	variance = float(sum(new_set))/float(len(new_set)-1)
+	return variance
+
+def pop_sd(x):
+	return math.sqrt(pop_var(x))
+
+def samp_sd(x):
+	return math.sqrt(samp_var(x))
+
